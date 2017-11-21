0effbfd35c7519b157e72f8c9c87c4063656caa9
Newton <engisaacnewton@gmail.com>
2017-11-21 00:54:15 -0300

    route util public

--- API/src/util/route-util.js
+++ API/src/util/route-util.js
@@ -1,4 +1,3 @@
 export function isPublic(req) {
-    return new RegExp('/api/auth').test(req.path) ||
-        new RegExp('/api/users').test(req.path);
+    return new RegExp('/api/auth').test(req.path) 
 }
